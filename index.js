528ae838bf17d6c3fbb171ae3c47d0e35633f273
Adam Niczyporuk <adam.niczyporukk@gmail.com>
2025-02-04 13:35:29 +0100

    Tutorial Data

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra')
 
-(async () => { 
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
 
+async function run() {
     const browser = await puppeteer.launch({headless: false}); 
-    const page = await browser.newPage();
+    const page = await browser.newPage()
+    await page.goto("https://fill.dev/form/credit-card-simple");
 
-    await page.goto('https://www.google.com');
+    let selector = "input[id='cc-name']";
+    await page.waitForSelector(selector);
+    await page.type(selector, "John Doe");
+
+    selector = "select[id='cc-type']";
+    await page.waitForSelector(selector);
+    await page.type(selector, "visa");
+
+    selector = "input[id='cc-number']";
+    await page.type(selector, "112312314141");
+
+    selector = "input[id='cc-number']";
     
-})();
+    selector = "input[id='cc-csc']";
+    await page.type(selector, "4242424242424242");
+
+    selector = "select[id='cc-exp-month']";
+    await page.select(selector, "12");
+    
+
+    selector = "select[id='cc-exp-year']";
+    await page.select(selector, "2025");
+
+    selector = "button[type='submit']";
+    await page.click(selector);
+}
+
+run();
