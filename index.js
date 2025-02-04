aa03135ebb86517f11352fd3429d04eebcc14f48
Adam Niczyporuk <adam.niczyporukk@gmail.com>
2025-02-04 12:31:50 +0100

    Add Script for running scripts

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const puppeteer = require('puppeteer');
 
 (async () => { 
 
-    const browser = await puppeteer.launch(); 
+    const browser = await puppeteer.launch({headless: false}); 
     const page = await browser.newPage();
 
-
+    await page.goto('https://www.google.com');
+    
 })();
