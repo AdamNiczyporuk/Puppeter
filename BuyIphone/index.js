c3a02d929111218de30360260521e90fc7ae4d8e
Adam Niczyporuk <adam.niczyporukk@gmail.com>
2025-02-08 14:25:07 +0100

    Add run and give page function

--- BuyIphone/index.js
+++ BuyIphone/index.js
@@ -1,5 +1,23 @@
 const puppeteer = require('puppeteer-extra')
-
 const StealthPlugin = require('puppeteer-extra-plugin-stealth')
-puppeteer.use(StealthPlugin())
-const locateChrome = require('chrome-location')
+puppeteer.use(StealthPlugin());
+
+const locateChrome = require('chrome-location');
+const {executablPath} = require('puppeteer');
+
+const url_16= "https://www.apple.com/pl/shop/buy-iphone/iphone-16-pro"
+
+
+async function givePage()
+{ 
+    const browser = await puppeteer.launch({ headless: false, executablePath: locateChrome});
+    let page = await browser.newPage()
+    return page;
+}
+
+async function run(){ 
+    let page = await givePage();
+    await page.goto(url_16);
+}
+
+run();
