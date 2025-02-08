55aa6a3abc964e7d215bd913127ad1e8d1d702c1
Adam Niczyporuk <adam.niczyporukk@gmail.com>
2025-02-08 13:53:22 +0100

    add chrome location

--- BuyIphone/index.js
+++ BuyIphone/index.js
@@ -2,3 +2,4 @@ const puppeteer = require('puppeteer-extra')
 
 const StealthPlugin = require('puppeteer-extra-plugin-stealth')
 puppeteer.use(StealthPlugin())
+const locateChrome = require('chrome-location')
